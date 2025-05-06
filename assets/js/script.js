29e2274fddaac260f800bf1ef8df5c9bee4679f3
FValenzuelaB <164669852+FValenzuelaB@users.noreply.github.com>
2025-05-06 14:15:44 +0000

    se añaden funciones js y google fonts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,42 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 
 $('#enviado').click(function(){
     alert("Gracias, El mensaje fue enviado correctamente!")
 })
 
+$('#cartta1').hover(function(){
+    $(this).fadeOut(400)
+    $(this).fadeIn(400)
+})
+$('#cartta2').hover(function(){
+    $(this).fadeOut(400)
+    $(this).fadeIn(400)
+})
+$('#cartta3').hover(function(){
+    $(this).fadeOut(400)
+    $(this).fadeIn(400)
+})
+$('#cartta4').hover(function(){
+    $(this).fadeOut(400)
+    $(this).fadeIn(400)
+})
+
+
+$("#github").on("click",function(){
+    window.open('https://github.com/','_blank');
+})
+
+$("#linkedin").on("click",function(){
+    window.open('https://www.linkedin.com/','_blank');
+})
+
+$("#twitter").on("click",function(){
+    window.open('https://x.com/','_blank');
+})
+
+$("#face").on("click",function(){
+    window.open('https://www.facebook.com/','_blank');
+})
