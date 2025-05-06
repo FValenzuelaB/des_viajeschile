f79cbf27b1b348fcf60ebf83ec90514833a4f9bf
FValenzuelaB <164669852+FValenzuelaB@users.noreply.github.com>
2025-05-06 01:01:08 +0000

    solo falta un par de popover

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+$('#enviado').click(function(){
+    alert("Gracias, El mensaje fue enviado correctamente!")
+})
+
